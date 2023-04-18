0645006c5b3aad7383d54ff89cce31e9ee065b92
Orjuela91 <jurgen.orjuela@unillanos.edu.co>
2023-04-17 23:30:09 -0500

    habilitar cors origin

--- src/main/java/com/disney/controller/PersonajeController.java
+++ src/main/java/com/disney/controller/PersonajeController.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/personajes")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonajeController {
 
     @Autowired
